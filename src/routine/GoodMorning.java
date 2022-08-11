7413796b612c5a3d261c95df1a74420c171aeb83
jsk33a <jsk33a@gmail.com>
2022-08-12 02:17:40 +0900

    bu

--- src/routine/GoodMorning.java
+++ src/routine/GoodMorning.java
@@ -9,19 +9,19 @@ public class GoodMorning {
 	static int total = 0;
 
     public static void main(String[] args) {
-        // 1. src로 구성할 수 있는 모든 부분집합을 출력하시오.
-        System.out.println("부분집합");
-        powerset();
-        System.out.println("----------------------------------");
-        
+//        // 1. src로 구성할 수 있는 모든 부분집합을 출력하시오.
+//        System.out.println("부분집합");
+//        powerset();
+//        System.out.println("----------------------------------");
+//        
 //        // 2. src에서 3개를 뽑아서 만들 수 있는 조합을 모두 출력하시오.
 //        System.out.println("조합");
 //         makeCombination(3,new char[3], 0);
 //         System.out.println("----------------------------------");
 //         
-//        // 3. src에서 3개를 뽑아서 만들 수 있는 순열을 모두 출력하시오.
-//        System.out.println("순열");
-//         makePermutation(3, new char[3], new boolean[src.length]);
+        // 3. src에서 3개를 뽑아서 만들 수 있는 순열을 모두 출력하시오.
+        System.out.println("순열");
+         makePermutation(3, new char[3], new boolean[src.length]);
     }
 
     static void powerset() {
